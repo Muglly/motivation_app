cdceb0d81ef9dd877f2eb843610294a770371833
Rafael Pereira <rpsouza.dev@gmail.com>
2023-07-14 14:13:20 -0300

    Implementing navigation between screens

--- app/src/main/java/com/example/motivation/UserActivity.kt
+++ app/src/main/java/com/example/motivation/UserActivity.kt
@@ -1,8 +1,10 @@
 package com.example.motivation
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import com.example.motivation.databinding.ActivityMainBinding
 import com.example.motivation.databinding.ActivityUserBinding
 
@@ -21,7 +23,18 @@ class UserActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onClick(view: View) {
         if (view.id == R.id.button_save) {
-            println("clicado")
+            handleSave()
+        }
+    }
+
+    private fun handleSave() {
+        val name = binding.editYourName.text.toString()
+
+        if (name != "") {
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        } else {
+            Toast.makeText(this, R.string.validation_mandatory_name, Toast.LENGTH_LONG).show()
         }
     }
 }
